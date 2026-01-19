a514333da31a5301f1660da83d81555ccc8de2bc
Logihertz Systems <logihertzsystems@gmail.com>
2026-01-20 00:30:37 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Logihertz Systems Official Update Script
 
+echo "Checking for system patches..."
+sleep 2
 
-
+# For now, we just tell the user they are up to date.
+# Later, you can change this to 'sudo apt update && sudo apt upgrade -y'
 echo "System is currently up to date."
 exit 0
